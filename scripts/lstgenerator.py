98d8570975981963beb5e8e3cf45a9ff3bf10526
Venom-Ocelot <marcelocavazos@me.com>
2022-03-14 11:48:35 -0400

    lstgenerator v.1.1 This simple script will create a file named "newlst.lst" and will append the path of each training set to the file. 1.1 Added an if statement to make sure the script does not see the "newlst.lst" script and try to incorporate it.

--- scripts/lstgenerator.py
+++ scripts/lstgenerator.py
@@ -3,7 +3,8 @@ import os
 new_lst = open ("newlst.lst", "w")
 
 for x in os.listdir():
-    new_lst.write(os.getcwd() + x + '\n')
+    if x != "newlst.lst":
+        new_lst.write(f"{os.getcwd()}/{x}\n")
 new_lst.close()
 
 print("A new .lst file has been created for use with SCRMSHAW.")
